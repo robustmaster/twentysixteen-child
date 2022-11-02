5576150c65d37dfbf99913480cab1695e898864a
robustmaster <109269923+robustmaster@users.noreply.github.com>
2022-11-02 15:23:48 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,19 @@
 <?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
+	$parenthandle = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+	$theme        = wp_get_theme();
+	wp_enqueue_style( $parenthandle,
+		get_template_directory_uri() . '/style.css',
+		array(),  // If the parent theme code has a dependency, copy it to here.
+		$theme->parent()->get( 'Version' )
+	);
 	wp_enqueue_style( 'child-style',
 		get_stylesheet_uri(),
-		array( 'parenthandle' ),
-		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+		array( $parenthandle ),
+		$theme->get( 'Version' ) // This only works if you have Version defined in the style header.
 	);
 }
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 
 function dmeng_get_https_avatar($avatar) {
 	//~ 替换为 https 的域名
