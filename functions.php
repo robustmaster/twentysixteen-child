1f286f1a917df131f55c8cc622a76e8c4121216b
robustmaster <109269923+robustmaster@users.noreply.github.com>
2022-11-02 14:52:46 +0800

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 'child-style',
+		get_stylesheet_uri(),
+		array( 'parenthandle' ),
+		wp_get_theme()->get( 'Version' ) // This only works if you have Version defined in the style header.
+	);
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function dmeng_get_https_avatar($avatar) {
+	//~ 替换为 https 的域名
+	$avatar = str_replace(array("www.gravatar.com", "0.gravatar.com", "1.gravatar.com", "2.gravatar.com", "secure.gravatar.com"), "gravatar.yipai.me", $avatar);
+	//~ 替换为 https 协议
+	$avatar = str_replace("http://", "https://", $avatar);
+	return $avatar;
+}
+add_filter('get_avatar', 'dmeng_get_https_avatar');
+
+add_filter( 'comment_excerpt_length', function($length) {
+    return 50;
+}, PHP_INT_MAX );
+
+function mytheme_custom_excerpt_length( $length ) {
+    return 140;
+}
+add_filter( 'excerpt_length', 'mytheme_custom_excerpt_length', 999 );
